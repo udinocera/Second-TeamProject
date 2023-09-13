ba904effd29c3adf65887586e32827819c45b99c
Sven <chanck.mds@gmail.com>
2023-09-14 00:18:46 +0200

    simple navbar template

---
+++ index/script.js
@@ -0,0 +1,34 @@
+const toggleNavButton = document.getElementById('toggle-nav');
+const navElements = document.querySelector('.nav-elements');
+
+const extraNavButton = document.getElementById('nav-extra-menu-button');
+const navExtraMenu = document.querySelector('.nav-extra-menu');
+
+const navHelpButton = document.getElementById('nav-help');
+const helpSection = document.querySelector('.help-section');
+
+const navProfileButton = document.getElementById('nav-profile');
+const ProfileSection = document.querySelector('.profile-section');
+
+// toogleNav
+toggleNavButton.addEventListener('click', () => {
+    navElements.classList.toggle('active');
+});
+
+// Extra nav
+
+extraNavButton.addEventListener('click', () => {
+    navExtraMenu.classList.toggle('active');
+});
+
+// Help
+
+navHelpButton.addEventListener('click', () => {
+    helpSection.classList.toggle('active');
+});
+
+// Profile
+
+navProfileButton.addEventListener('click', () => {
+    ProfileSection.classList.toggle('active');
+});
